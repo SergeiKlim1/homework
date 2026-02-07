e29926ed165ac626c2c4fb9781421cf50ad9a272
Sergei Klimenko <sergey_sergey_38@mail.ru>
2026-02-07 22:23:15 +0800

    add function for sort by date

--- src/processing.py
+++ src/processing.py
@@ -17,3 +17,13 @@ def filter_by_state(transactions: List[Dict[str, Any]], state = "EXECUTED") -> L
 
     return transactions_list
 
+
+def sort_by_date(transactions: List[Dict[str, Any]], type_sort: Bool = True) -> List[Dict[str, Any]]:
+    """"
+    Функция, которая принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание) и возвращает
+    новый список отсортированный по дате
+    """
+    sorted_list_by_date = sorted(transactions, key=lambda x: datetime.strptime(x["date"], '%Y-%m-%dT%H:%M:%S.%f'), reverse=type_sort)
+
+    return sorted_list_by_date
