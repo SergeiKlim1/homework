0df790c9bb0f9a725df97aa42167e7c667314361
Sergei Klimenko <sergey_sergey_38@mail.ru>
2026-02-12 22:24:23 +0800

    fix masks.py and add tests for masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,29 @@
-def get_mask_card_number(card_number: str) -> str:
+def get_mask_card_number(card_number: str | int) -> str:
     """Функция, которая принимает и маскирует номер карты"""
-    mask_card = card_number[0:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
-    mask_card_number = " ".join(
-        [mask_card[i : i + 4] for i in range(0, len(mask_card), 4)]
-    )
-    return mask_card_number
+    card_number_str = str(card_number)
+    if len(card_number_str) == 16:
+        mask_card = card_number_str[0:6] + (len(card_number_str[6:-4]) * "*") + card_number_str[-4:]
+        mask_card_number = " ".join(
+            [mask_card[i : i + 4] for i in range(0, len(mask_card), 4)]
+        )
+        return mask_card_number
+    elif len(card_number_str) == 0:
+        raise ValueError("Строка пустая")
+    else:
+        raise ValueError("Введены не корректные данные")
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_account(account_number: str | int) -> str:
     """Функция, которая принимает и маскирует номер счета"""
-    mask_account_number = account_number.replace(account_number[0:-4], "**")
-    return mask_account_number
+    account_number_str = str(account_number)
+    if len(account_number_str) == 20:
+        mask_account_number = account_number_str.replace(account_number_str[0:-4], "**")
+        return mask_account_number
+    elif len(account_number_str) == 0:
+        raise ValueError("Строка пустая")
+    else:
+        raise ValueError("Введены не корректные данные")
+
+numbers_card = []
+if __name__ == "__main__":
+    print(get_mask_account(numbers_card))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,20 +1,34 @@
+from sys import exc_info
+
 import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-@pytest.fixture
-def account_number():
-    return "73654108430135874305"
+@pytest.mark.parametrize("card_number, expected", [("7000792289606361", "7000 79** **** 6361"),
+                                                   (1596837868705199, "1596 83** **** 5199")])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+@pytest.mark.parametrize("incorrect_number, expected", [("", "Строка пустая"),
+                                                             ("fhd", "Введены не корректные данные"),
+                                                             ([], "Введены не корректные данные")])
+def test_get_mask_incorrect_card_number(incorrect_number, expected):
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number(incorrect_number)
+    assert str(exc_info.value) == expected
+
+@pytest.mark.parametrize("incorrect_number, expected", [("", "Строка пустая"),
+                                                             ("fhd", "Введены не корректные данные"),
+                                                             ([], "Введены не корректные данные")])
+def test_get_mask_incorrect_account(incorrect_number, expected):
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account(incorrect_number)
+    assert str(exc_info.value) == expected
 
 
-@pytest.fixture
-def card_number():
-    return "7000792289606361"
+@pytest.mark.parametrize("account_number, expected", [("73654108430135874305", "**4305"),
+                                                      ("64686473678894779589", "**9589"),
+                                                      ])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
 
-
-def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == "7000 79** **** 6361"
-
-
-def test_get_mask_account(account_number):
-    assert get_mask_account(account_number) == "**4305"
