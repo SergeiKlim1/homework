e0cb9ccc99988f97c9dfc40f0b058485b4ced934
Sergei Klimenko <sergey_sergey_38@mail.ru>
2026-02-14 00:09:40 +0800

    fix prcessing.py and add tests for processing.py

--- src/processing.py
+++ src/processing.py
@@ -11,12 +11,18 @@ def filter_by_state(
     словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению
     """
+    transactions_list_with_state = []
     transactions_list = []
 
+
     for transaction in transactions:
+        for key in transaction:
+            if key == "state":
+                transactions_list_with_state.append(transaction)
+
+    for transaction in transactions_list_with_state:
         if transaction["state"] == state:
             transactions_list.append(transaction)
-
     return transactions_list
 
 
@@ -28,10 +34,13 @@ def sort_by_date(
     задающий порядок сортировки (по умолчанию — убывание) и возвращает
     новый список отсортированный по дате
     """
-    sorted_list_by_date = sorted(
+    try:
+        sorted_list_by_date = sorted(
         transactions,
-        key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"),
-        reverse=type_sort,
-    )
+        key=lambda x: (datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), x["id"]),
+        reverse=type_sort)
+        return sorted_list_by_date
+    except ValueError:
+        raise ValueError("Дата не в iso формате")
 
-    return sorted_list_by_date
+
