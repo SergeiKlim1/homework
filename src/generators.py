c60085af722f2619b7327bb160b980b9eb9ec3af
Sergei Klimenko <sergey_sergey_38@mail.ru>
2026-05-24 15:52:48 +0800

    added generator return descriptions of each operation

--- src/generators.py
+++ src/generators.py
@@ -1,12 +1,23 @@
-from typing import List, Any, Generator
+from typing import Dict, List, Any, Generator
 
 
-def filter_by_currency(list_dict: List [dict, any], currency: str) -> Generator[List[dict, Any], None, None]:
+def filter_by_currency(list_dict: List [Dict[str, Any]], currency: str) -> Generator[List[dict, Any], None, None]:
 
-    """"Функция возвращающая итератор, который поочередно выдает транзакции,
+    """Функция возвращающая итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной"""
 
 
     filtered_list_dict = (transaction for transaction in list_dict if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency)
     return filtered_list_dict
+
+
+def transaction_descriptions(list_dict: List [Dict[str, Any]]) -> Generator[str, None, None]:
+
+    """Генератор, который принимает список словарей с транзакциями и
+    возвращает описание каждой операции по очереди"""
+
+
+    for transaction in list_dict:
+        description = transaction.get("description", "")
+        yield description
 
